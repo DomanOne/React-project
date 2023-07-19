bc8ddd23065fd0c4d75e733f05c0a7ec0114d5b1
igordoman0@gmail.com <igordoman0@gmail.com>
2023-07-19 17:25:31 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { icons } from "react-icons";
+import { FcPhone } from "react-icons/fc";
+import { FcLike } from "react-icons/fc";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h3 className="contacts">Contact:</h3>
+
+			<p>
+				<FcPhone />
+				Tel:0700-880-749
+			</p>
+			<p>
+				<FcLike />
+				Adress:Butt-hole 99 Myslowice
+			</p>
+		</div>
+	);
 }
 
 export default App;
